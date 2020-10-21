72714b1de8942c92d102d342dfed22f279b92d42
wennitao <wennitao@126.com>
2020-10-21 21:34:21 +0800

    update

--- math/Poly.cpp
+++ math/Poly.cpp
@@ -2,12 +2,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-bool Finish_read;
-template<class T>inline void read(T &x){Finish_read=0;x=0;int f=1;char ch=getchar();while(!isdigit(ch)){if(ch=='-')f=-1;if(ch==EOF)return;ch=getchar();}while(isdigit(ch))x=x*10+ch-'0',ch=getchar();x*=f;Finish_read=1;}
-template<class T>inline void print(T x){if(x/10!=0)print(x/10);putchar(x%10+'0');}
-template<class T>inline void writeln(T x){if(x<0)putchar('-');x=abs(x);print(x);putchar('\n');}
-template<class T>inline void write(T x){if(x<0)putchar('-');x=abs(x);print(x);}
-/*================Header Template==============*/
 const int mod=998244353,inv2=(mod+1)>>1,mrt=31596;
 namespace {
     inline int Add(const int &x,const int &y) {
